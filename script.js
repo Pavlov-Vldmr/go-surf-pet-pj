79c67590028a314c3614897e9a4aa6141e365143
Pavlov-Vldmr <vpavlov95007@gmail.com>
2024-11-14 03:13:02 +0300

    hero section swiper slider

--- script.js
+++ script.js
@@ -6,10 +6,37 @@ $(document).ready(function () {
   mm.text(new Date().getMonth() + 1);
   yy.text(new Date().getFullYear());
 
+  let location = ["North Shore", "South Shore", "West Shore", "East Shore"];
+
   var swiper = new Swiper(".mySwiper", {
+    effect: "fade",
+    loop: "true",
+    speed: "500",
+    fadeEffect: {
+      crossFade: true,
+    },
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      nextEl: ".swiper-button-prev",
+      prevEl: ".swiper-button-next",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+
+      renderBullet: function (index, className) {
+        return (
+          '<div class="' +
+          className +
+          '">' +
+          "<span>0" +
+          (index + 1) +
+          "</span>" +
+          "<span>" +
+          location[index] +
+          "</span>" +
+          "</div>"
+        );
+      },
     },
   });
 });
